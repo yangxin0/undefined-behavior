382e27695e7608b34b413e8d2babb75b3abc0d28
Xin Yang <yangxin0@outlook.com>
2023-04-09 18:10:19 +0800

    nginx: port from server

---
+++ nginx/build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: nginx-server($0) TAG (eg: latest, v0.1)"
+	exit 1
+fi
+
+DIR="$(dirname "${BASH_SOURCE[0]}")"
+echo $DIR
+docker build -t nginx-server:${1} -f ${DIR}/Dockerfile ${DIR}/../server
+
