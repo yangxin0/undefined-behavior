848daefddf72e156e6a697ee21b3229a24f60a96
Xin Yang <yangxin0@outlook.com>
2023-04-15 19:16:17 +0800

    server: 1$ free trial

--- server/chat/signals.py
+++ server/chat/signals.py
@@ -1,7 +1,17 @@
-from django.db.models.signals import post_migrate
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Setting
+from .models import Deposit, DespoitSource, DespoitCurrency, Balance
+from django.contrib.auth.models import User
 
-@receiver(post_migrate)
-def load_default_settings(sender, **kwargs):
-    pass
+
+@receiver(post_save, sender=User)
+def handle_user_post_save(sender, instance, created, **kwargs):
+    if created == False:
+        return
+    # 1$ free trial
+    Deposit.objects.create(user=instance,
+                           amount=1,
+                           exchange=1,
+                           source=DespoitSource.TRIAL,
+                           currency=DespoitCurrency.USD)
+    Balance.objects.create(user=instance, usd_amount=1)
